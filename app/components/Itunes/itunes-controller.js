4ddd0eeba791a0d447010cabd4815c358a16a923
Tavia Sterling <TaviaSterling@outlook.com>
2018-08-10 15:20:50 -0600

    change to get itunes api

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -28,7 +28,14 @@ for (let i = 0; i < songs.length; i++) {
   }
   document.getElementById('songs').innerHTML = template
 }
-
+document.addEventListener('play', function(e){
+  var audios = document.getElementsByTagName('audio');
+  for(var i = 0, len = audios.length; i < len;i++){
+      if(audios[i] != e.target){
+          audios[i].pause();
+      }
+  }
+}, true);
 
 
 
